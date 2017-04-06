86f9c8539fa5865065a0605751d433d86a90b9e3
mbu13 <matthewbu13@gmail.com>
2017-04-06 17:51:21 -0400

    Added postgis to Vagrant script and added API test

--- rpi_csdt_community/tests.py
+++ rpi_csdt_community/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test.client import Client
 from django.conf import settings
+import urllib2
+import json
 
 from BeautifulSoup import BeautifulSoup, SoupStrainer
 
@@ -18,51 +20,72 @@ class UrlTests(StaticLiveServerTestCase):
         self.client = Client()
         self.client.login(username='temporary', password='temporary')
         self.visited = {}
-
+    
     def test_all_site_links(self, url='/'):
         if url in self.visited:
             return
         self.visited[url] = True
-
+        
         # Ignore URL's that point elsewhere
         if url.startswith('http://') or url.startswith('http://') or url.startswith('https://') or url.startswith('//'):
             return
 
-        # Ignore URL that simply point to media...
-        if url.startswith(settings.MEDIA_URL):
-            return
-
+# Ignore URL that simply point to media...
+if url.startswith(settings.MEDIA_URL):
+    return
+        
         # We need the HTTP_REFERER here to get past Django-likes checking
         response = self.client.get(url, **{'HTTP_REFERER':url})
-
+        
         """
-        import sys
-        sys.stdout.write(url + '->' + repr(response.status_code)+'\n')
-        sys.stdout.flush()
-        """
-
+            import sys
+            sys.stdout.write(url + '->' + repr(response.status_code)+'\n')
+            sys.stdout.flush()
+            """
+        
         self.assertTrue(response.status_code == 200 or response.status_code == 302, msg="Got code %s on %s" % (response.status_code, url))
-
+        
         for link in BeautifulSoup(response.content, parseOnlyThese=SoupStrainer('a')):
             if any('href' in el for el in link.attrs):
                 self.test_all_site_links(link['href'])
 
-    """
+"""
     # For the time being, this test doesn't serve any purpose
     def test_captcha_works(self):
-        url = '/accounts/register/'
-
-        response = self.client.get(url)
-
-        self.assertEqual(response.status_code, 200)
-
-        soup = BeautifulSoup(response.content)
-        images = soup.findAll(attrs={'class': 'captcha'})
-
-        for image in images:
-            import sys
-            sys.stdout.write(str(image))
-            sys.stdout.flush()
-            response = self.client.get(image.get('src'))
-            self.assertEqual(response.status_code, 200)
+    url = '/accounts/register/'
+    
+    response = self.client.get(url)
+    
+    self.assertEqual(response.status_code, 200)
+    
+    soup = BeautifulSoup(response.content)
+    images = soup.findAll(attrs={'class': 'captcha'})
+    
+    for image in images:
+    import sys
+    sys.stdout.write(str(image))
+    sys.stdout.flush()
+    response = self.client.get(image.get('src'))
+    self.assertEqual(response.status_code, 200)
     """
+
+class APITests(TestCase):
+    
+    def testLinks(self):
+        # Timeout limit in seconds
+        limit = 5
+        
+        # Verify root itself is okay
+        response = urllib2.urlopen('https://dev1.csdt.rpi.edu/api-gis/', timeout = limit)
+        self.assertTrue(response.getcode(), 200);
+        
+        data = json.load(response)
+        
+        # Go through all links to test okay and returning some data
+        for key in data:
+            url = data.get(key)
+            
+            response = urllib2.urlopen(url, timeout = limit)
+            
+            self.assertTrue(response.getcode(), 200);
+            self.assetTrue(len(json.load(response)) > 0)
